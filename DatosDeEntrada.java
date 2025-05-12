3fe8d7b7150ddba36fdd99c7ad3a94b7bece64d6
KASHlMA <gustavoanayalopez32@gmail.com>
2025-05-12 10:48:37 -0600

    o no curi?

--- DatosDeEntrada.java
+++ DatosDeEntrada.java
@@ -2,11 +2,12 @@ import javax.swing.JOptionPane;
 
 public class DatosDeEntrada{
     public static void main(String[] args){
+        
         String nomnbre = JOptionPane.showInputDialog("Ingresa tu nombre");
         String apellidoP = JOptionPane.showInputDialog("Ingresa tu apellido paterno");
         String apellidoM = JOptionPane.showInputDialog("Ingresa tu apellido materno");
-
-        System.out.println("Tu nombre es: "+nomnbre+" y tus apellidos "+apellidoP+""+apellidoM);
+//printl del nombre y los apellidos o no curi
+        System.out.println("Tu nombre es: "+nomnbre+" y tus apellidos "+apellidoP+" "+apellidoM);
 
 
     }
